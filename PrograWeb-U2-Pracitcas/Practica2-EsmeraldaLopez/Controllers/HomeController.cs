0b7bc9a080b1d7a14dc7053ee522149c27a2013b
Esmeralda Lopez Muñoz <171G0234@rcarbonifera@tecnm.mx>
2020-09-10 20:21:43 -0500

    Trabajo terminado. Programación web. Practica suma de números.

---
+++ PrograWeb-U2-Pracitcas/Practica2-EsmeraldaLopez/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Practica2_EsmeraldaLopez.Models;
+
+namespace Practica2_EsmeraldaLopez.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ObtenerResultados(Numeros n)
+        {
+            var respuesta = n.n1 + n.n2;
+            return View(respuesta);
+        }
+    }
+}
---
+++ PrograWeb-U2-Pracitcas/Practica2-EsmeraldaLopez/Models/Numeros.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practica2_EsmeraldaLopez.Models
+{
+    public class Numeros
+    {
+        public int n1 { get; set; }
+        public int n2 { get; set; }
+    }
+}
