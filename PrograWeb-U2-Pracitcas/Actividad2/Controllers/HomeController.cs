565af0db213c04886ebb344302747b715066dccd
Esmeralda Lopez Muñoz <171G0234@rcarbonifera@tecnm.mx>
2020-09-13 22:03:17 -0500

    Actividad 2. BDD con ASP .NET. Estados de MX

---
+++ PrograWeb-U2-Pracitcas/Actividad2/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Actividad2.Models;
+
+namespace Actividad2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            mexicoContext context = new mexicoContext();
+            var estados = context.Estados.OrderBy(x => x.Nombre).ToList();
+
+
+            return View(estados);
+        }
+    }
+}
---
+++ PrograWeb-U2-Pracitcas/Actividad2/Models/Estados.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Actividad2.Models
+{
+    public partial class Estados
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Abrev { get; set; }
+    }
+}
